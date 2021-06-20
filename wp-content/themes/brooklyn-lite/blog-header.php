953c99f5d8f16029f8b246d2f9d26da30c363a27
Anderps <31406020+Anderps@users.noreply.github.com>
2021-06-20 20:44:20 +0200

    Restoring old

--- wp-content/themes/brooklyn-lite/blog-header.php
+++
@@ -1,9 +0,0 @@
-<section class="brklyn-page background-bg">
-    <div class="brklyn-gray-bg">
-        <div class="section-padding">
-            <div class="container">
-                <?php brooklyn_lite_breadcrumb_trail();?>
-            </div><!-- /.container -->
-        </div><!-- /.section-padding -->
-    </div><!-- /.overlay -->
-</section>
